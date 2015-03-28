4fea81ac2860afac6410e24a20275926ab733011
Ludovic Vannoorenberghe <ludovic.v@me.com>
2015-03-28 15:55:30 +0100

    tile directive

---
+++ app/scripts/controllers/tile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name challengeApp.controller:TileCtrl
+ * @description
+ * # TileCtrl
+ * Controller of the challengeApp
+ */
+angular.module('challengeApp')
+  .controller('TileCtrl', function ($scope) {
+
+    $scope.tile = {
+      title: 'hello',
+      label: 'label'
+    };
+    
+  });
---
+++ app/scripts/directives/tile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name challengeApp.directive:tile
+ * @description
+ * # tile
+ */
+angular.module('challengeApp')
+  .directive('tile', function () {
+    return {
+      templateUrl: '/views/tile.html',
+    };
+  });
